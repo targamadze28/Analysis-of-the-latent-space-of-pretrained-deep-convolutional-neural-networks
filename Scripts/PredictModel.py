0630c52291708740f035960a26667b1504f7eeee
Dmitry Targamadze <targamadze28@mail.ru>
2021-02-09 18:23:23 +0300

    Update PredictModel.py

--- Scripts/PredictModel.py
+++ Scripts/PredictModel.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  6 15:07:24 2020
 
-@author: vsevolod
-"""
 
 import numpy as np
+import matplotlib.pyplot as plt
 import h5py as h5 
 
 import os.path as fs
 
+import keras
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dense, \
   SeparableConv2D, Dropout, concatenate , Conv2DTranspose, Layer
-from keras.models import Model
-from keras.datasets import mnist
-from keras import models
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras import Sequential
-from keras.models import load_model
-
-
-import keras
-from keras.callbacks import TensorBoard
 from keras import backend as K
-import numpy as np
-import matplotlib.pyplot as plt
-import h5py
-from sklearn.model_selection import train_test_split
+from keras.models import Model, import load_model
+from tensorflow.python.keras import Sequential
 
 #%%
 def readHDF5file(PathToSave, SavedFileName, list_group_name):
@@ -42,7 +28,6 @@ def readHDF5file(PathToSave, SavedFileName, list_group_name):
 """
 Image processing
 """
-
 def normalization(images):
   max_value = np.max(images)
   images = images/max_value
@@ -129,26 +114,6 @@ def ezConvAutoEncoderForMnist():
   decoded = Conv2D(4, (3, 3), activation='softmax', padding='same')(x)
   autoencoder = Model(input_img, decoded)
   return autoencoder
-
-
-def CustomConvAutoEncoderForMnist():
-  input_img = Input(shape=(64, 64, 3))  # adapt this if using `channels_first` image data format
-
-  x = Conv2D(8, (3, 3), activation='relu', padding='same')(input_img)
-  x = MaxPooling2D((2, 2))(x)
-  x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-  x = MaxPooling2D((2, 2))(x)
-  x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-  encoded = MaxPooling2D((2, 2))(x)
-  
-  x = Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='relu')(encoded)
-  x = Conv2DTranspose(16, (3, 3), strides=(2, 2), activation='relu')(x)
-  x = Conv2DTranspose(8, (3, 3), strides=(2, 2), activation='relu')(x)
-  x = Conv2D(8, (6, 6))(x)
-  decoded = Conv2D(4, (3, 3), activation='softmax')(x)
-  autoencoder = Model(input_img, decoded)
-  return autoencoder
-
 
 def Unet():
   input_img = Input(shape=(64, 64, 3)) 
@@ -324,75 +289,11 @@ def UnetWithSeparableConvСircumcised():
   conv10 = SeparableConv2D(4, 1, activation = 'softmax')(conv9)
   autoencoder = Model(inputs=input_img, outputs=conv10)
   return autoencoder
-
-
-def AlexNet():
-  input_img = Input(shape=(64, 64, 3)) 
-
-  conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-  conv1 = Dropout(0.2)(conv1)
-  conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv1)
-  pool1 = MaxPooling2D((2, 2))(conv1)
-  
-  conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(pool1)
-  conv2 = Dropout(0.2)(conv2)
-  conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv2)
-  pool2 = MaxPooling2D((2, 2))(conv2)
-  
-  conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool2)
-  conv3 = Dropout(0.2)(conv3)
-  conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv3)
-
-  up1 = concatenate([UpSampling2D((2, 2))(conv3), conv2], axis=-1)
-  conv4 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1)
-  conv4 = Dropout(0.2)(conv4)
-  conv4 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv4)
-  
-  up2 = concatenate([UpSampling2D((2, 2))(conv4), conv1], axis=-1)
-  conv5 = Conv2D(16, (3, 3), activation='relu', padding='same')(up2)
-  conv5 = Dropout(0.2)(conv5)
-  conv5 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv5)
-  
-  out = Conv2D(4, (1, 1) , padding='same')(conv5)
-  autoencoder = Model(inputs=input_img, outputs=out)
-  return autoencoder
-
-def AlexNetSeparable():
-  input_img = Input(shape=(64, 64, 3)) 
-
-  conv1 = SeparableConv2D(32, (3, 3), activation='relu', padding='same')(input_img)
-  conv1 = Dropout(0.2)(conv1)
-  conv1 = SeparableConv2D(32, (3, 3), activation='relu', padding='same')(conv1)
-  pool1 = MaxPooling2D((2, 2))(conv1)
-  
-  conv2 = SeparableConv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-  conv2 = Dropout(0.2)(conv2)
-  conv2 = SeparableConv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-  pool2 = MaxPooling2D((2, 2))(conv2)
-  
-  conv3 = SeparableConv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-  conv3 = Dropout(0.2)(conv3)
-  conv3 = SeparableConv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-
-  up1 = concatenate([UpSampling2D((2, 2))(conv3), conv2], axis=-1)
-  conv4 = SeparableConv2D(64, (3, 3), activation='relu', padding='same')(up1)
-  conv4 = Dropout(0.2)(conv4)
-  conv4 = SeparableConv2D(64, (3, 3), activation='relu', padding='same')(conv4)
-  
-  up2 = concatenate([UpSampling2D((2, 2))(conv4), conv1], axis=-1)
-  conv5 = SeparableConv2D(32, (3, 3), activation='relu', padding='same')(up2)
-  conv5 = Dropout(0.2)(conv5)
-  conv5 = SeparableConv2D(32, (3, 3), activation='relu', padding='same')(conv5)
-  
-  out = SeparableConv2D(4, (1, 1) , padding='same')(conv5)
-  autoencoder = Model(inputs=input_img, outputs=out)
-  return autoencoder
 #%%
 
-PathToDataSet = '/home/vsevolod/Desktop/Dymas/DataSets'
-NameDataSet = 'TrainDataSet.hdf5'
-NameTestDataSet = 'TestDataSet.hdf5'
-
+PathToDataSet = ''
+NameDataSet = ''
+NameTestDataSet = ''
 PathToModel = ''
 
 fileData = readHDF5file(PathToDataSet, NameTestDataSet,\
@@ -410,30 +311,19 @@ weights_CE[:3] = 4
 weights_focal = np.ones((4,))*0.25
 weights_focal[:3] = 0.75
   
-NN_list = [ezConvAutoEncoderForMnist(), CustomConvAutoEncoderForMnist(), \
-           DymasUnet(), DymasUnetСircumcised(), DymasUnetWithSeparableConv(),\
-             DymasUnetWithSeparableConvСircumcised()]
-
-loss_list = [weighted_categorical_crossentropy(weights_CE), categorical_focal_loss(weights_focal),\
-             dice_loss]
+NN_list = []
+loss_list = []
+metrics = []
   
-metrics = [weighted_categorical_crossentropy(weights_CE),\
-           categorical_focal_loss(weights_focal),\
-           iou_loss_core,\
-           dice_loss]
+name_loss_list = ['']
   
-name_loss_list = ['weighted_categorical_crossentropy', 'categorical_focal_loss',\
-             'dice_loss']
-  
-name_NN_list = ['ezConvAutoEncoderForMnist', 'CustomConvAutoEncoderForMnist', \
-           'DymasUnet', 'DymasUnetСircumcised', 'DymasUnetWithSeparableConv',\
-             'DymasUnetWithSeparableConvСircumcised', 'AlexNet', 'AlexNetSeparable']
+name_NN_list = ['']
 
 precision = np.empty((len(NN_list), len(loss_list), 2), dtype = np.float32)
 namemodel = np.empty((len(NN_list), len(loss_list)))
 for iter_NN in range(len(NN_list)):
   for iter_loss in range(len(loss_list)):
-    model = load_model(fs.join('/data/vsevolod/DmiDiplom/Models1000sample',\
+    model = load_model(fs.join('',\
                                'model_%s_%s.hdf5'%(name_NN_list[iter_NN], name_loss_list[iter_loss])),\
                    custom_objects = {'loss':loss_list[iter_loss], 'weighted_categorical_crossentropy':weighted_categorical_crossentropy(weights_CE),\
                                      'dice_loss':dice_loss,\
@@ -444,13 +334,13 @@ for iter_NN in range(len(NN_list)):
     precision[iter_NN, iter_loss, 1] = 1 - result[-2]
     namemodel[iter_NN, iter_loss] = 'model_%s_%s.hdf5'%(name_NN_list[iter_NN], name_loss_list[iter_loss])
 
-ff = h5.File(fs.join('/data/vsevolod/DmiDiplom/Models1000sample', 'predict_result.hdf5'))
+ff = h5.File(fs.join('', 'predict_result.hdf5'))
 ff.create_dataset('precision', data = precision)
 ff.create_dataset('namemodel', data = namemodel)
 ff.close()
 
 """
-Mdels Predictions
+Models Predictions
 """
 
 namemodel = np.empty((len(NN_list), len(loss_list)), np.chararray)
@@ -466,8 +356,8 @@ for iter_NN in range(len(NN_list)):
     print();print()
 
 
-model = load_model(fs.join('/home/vsevolod/Desktop/Dymas/Models',\
-                               'model_DymasUnetWithSeparableConvСircumcised_dice_loss.hdf5'),\
+model = load_model(fs.join('',\
+                           ''),\
                    custom_objects = {'loss':loss_list[-1], 'weighted_categorical_crossentropy':weighted_categorical_crossentropy(weights_CE),\
                                      'dice_loss':dice_loss,\
                                        'categorical_focal_loss_fixed':categorical_focal_loss(weights_focal),\
@@ -475,48 +365,14 @@ model = load_model(fs.join('/home/vsevolod/Desktop/Dymas/Models',\
 
 
 pred = model.predict(images[:100])
-
-print(pred.shape)
-
-for i in range(4):
-  plt.imshow(pred[1, :, :, i])
-  plt.show()
   
-ff = h5.File(fs.join('/home/vsevolod/Desktop/Dymas/DataSets', 'predictFive.hdf5'), 'r')
+ff = h5.File(fs.join(''), 'r')
 pred = ff['precision'][...]
 ff.close()
 
-print(pred.shape)
-
-for i in range(3):
-  for j in range(2):
-    print(pred[i, j, :, 0])
-
-name_loss_list = ['weighted_categorical_crossentropy', 'categorical_focal_loss',\
-             'dice_loss']
-  
-name_NN_list = ['ezConvAutoEncoderForMnist', 'CustomConvAutoEncoderForMnist', \
-           'DymasUnet', 'DymasUnetСircumcised', 'DymasUnetWithSeparableConv',\
-             'DymasUnetWithSeparableConvСircumcised']
+name_loss_list = ['']
+name_NN_list = ['']
 
 for i, nn_name in enumerate(name_NN_list):
   for j, loss_name in enumerate(name_loss_list):
     print(nn_name, loss_name, pred[i, j])
-
-plt.plot(np.arange(9), pred[0, 0, :, 0])
-plt.plot(np.arange(9), pred[0, 0, :, 1])
-plt.show()
-
-plt.plot(np.arange(9), pred[1, 0, :, 0])
-plt.plot(np.arange(9), pred[1, 0, :, 1])
-plt.show()
-
-plt.plot(np.arange(9), pred[2, 0, :, 0])
-plt.plot(np.arange(9), pred[2, 0, :, 1])
-plt.show()
-
-
-
-((-1.)/(24.))*(1.5 - 2)*(1.5 - 3)*(1.5 - 4) + (1.5 - 0)*(1.5 - 3)*(1.5 - 4) - (8./3.)*(1.5 - 0)*(1.5 - 2)*(1.5 - 4) + 2*(1.5 - 0)*(1.5 - 2)*(1.5 - 3)
-
-np.power(2., 1.5)
